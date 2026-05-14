5a1edd8f06c7a8426a680004357e6ed4cc48fba3
Muhammad Ilman <muhammadilman836@gmail.com>
2026-05-14 12:55:35 +0700

    Refactor input fields to remove default values
    Removed default values from number inputs for user flexibility.

--- house_price.py
+++ house_price.py
@@ -8,13 +8,13 @@ scaler = pickle.load(open('scaler.sav', 'rb'))
 
 st.title('House Price Prediction')
 
-num_bedrooms = st.number_input('Number of Bedrooms', min_value=0, max_value=10, value=3)
-num_bathrooms = st.number_input('Number of Bathrooms', min_value=0.0, max_value=10.0, value=2.0, step=0.5)
-square_footage = st.number_input('Square Footage', min_value=100, max_value=10000, value=1500)
-lot_size = st.number_input('Lot Size (in acres)', min_value=0.0, max_value=10.0, value=0.5, step=0.1)
-year_built = st.number_input('Year Built', min_value=1800, max_value=2024, value=2000)
-garage_size = st.number_input('Garage Size (number of cars)', min_value=0, max_value=5, value=2)
-neighborhood_quality = st.number_input('Neighborhood Quality (1-10)', min_value=1, max_value=10, value=7)
+num_bedrooms = st.number_input('Number of Bedrooms')
+num_bathrooms = st.number_input('Number of Bathrooms')
+square_footage = st.number_input('Square Footage')
+lot_size = st.number_input('Lot Size (in acres)')
+year_built = st.number_input('Year Built')
+garage_size = st.number_input('Garage Size')
+neighborhood_quality = st.number_input('Neighborhood Quality (1-10)')
 
 prediction = ''
 
